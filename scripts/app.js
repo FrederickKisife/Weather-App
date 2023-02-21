603b728472361a7e63c2cf00237b19f5392a6edf
FrederickKisife <kilafrederick@gmaul.com>
2023-02-21 09:17:59 -0800

    update the city ui in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,11 @@ const cityForm = document.querySelector("form");
 cityForm.addEventListener("submit", e =>{
     // prevent default 
     e.preventDefault();
+
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update the ui with new city
+
 })
