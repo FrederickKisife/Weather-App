d4c4608ce40c3ef5b5031ff4033f1ad09fbaf3d2
FrederickKisife <kilafrederick@gmaul.com>
2023-02-21 08:42:48 -0800

    api keys set up

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,13 @@
 const key = "Xp9sUrWsROTILnrT2dPJb2FsJFyhTg6b";
+
+const getCity = async (city)=>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data);
+}
+
+getCity("manchester");
